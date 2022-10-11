eb4295eb3ac5789bed6a3a7720b65128b0b93fa2
mateus040 <mateusgabrielmoreno264@gmail.com>
2022-10-11 13:42:53 -0300

    adicionando arquivo login

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/style_login.css">
+</head>
+<body>
+    
+    <div class="corpo-form">
+        <h1> Fazer Login </h1>
+        <form method="POST" action="processamento.php">
+
+            <input type="email" placeholder="Usuário">
+            <input type="password" placeholder="Senha">
+            <input type="submit" value="ACESSAR">
+            <a href="">Ainda não se registrou?<strong> Cadastre-se! </strong> </a>
+
+        </form>
+    </div>
+
+</body>
+</html>
