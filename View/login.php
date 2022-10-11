75a3253dce42424f5f33635a50913aa9f44063d4
mateus040 <mateusgabrielmoreno264@gmail.com>
2022-10-11 19:43:32 -0300

    fazendo alterações

---
+++ View/login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/style_login.css">
+</head>
+<body>
+
+    <form method="POST" action="processamento.php">
+        <div class="main-login">
+            <div class="left-login">
+                <img src="images/logo1.png" class="left-login-image">
+            </div>
+            <div class="right-login">
+                <div class="card-login">
+                    <h1>LOGIN</h1>
+                    <div class="textfield">
+                        <input type="text" name="usuario" placeholder="Usuário">
+                    </div>
+                    <div class="textfield">
+                        <input type="password" name="senha" placeholder="Senha">
+                    </div>
+                    <button type="submit" class="btn-login">Login</button>
+                    <a href="/cadastrar">Ainda não se cadastrou? <strong> Cadastre-se! </strong> </a>
+                </div>
+            </div>
+        </div>
+    </form>
+    
+</body>
+</html>
