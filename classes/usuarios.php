054d11888382f74a8e97c6404209e605e2ea4d21
mateus040 <mateusgabrielmoreno264@gmail.com>
2022-10-11 20:20:15 -0300

    adicionando o arquivo usuarios

---
+++ classes/usuarios.php
@@ -0,0 +1,68 @@
+<?php
+
+class Usuario
+{
+    private $conexao;
+    public $msgErro = "";
+
+    public function __construct()
+    {
+        include_once '../sql/MySQL.php';
+
+        $this->conexao = new MySQL();
+    }
+
+
+    /*public function conectar($nome, $host, $usuario, $senha)
+    {
+        global $pdo;
+
+        try 
+        {
+            $pdo = new PDO("mysql:db_techpower=".$nome.";host=".$host,$usuario,$senha);
+        }
+        catch (PDOException $e)
+        {
+            $msgErro = $e->getMessage();
+        }
+
+    }
+    */
+
+    public function cadastrar($nome, $telefone, $email, $senha)
+    {
+        global $conexao;
+
+        // Verificar se já existe o email cadastrado
+        $sql = $conexao->prepare("SELECT id_usuario FROM usuarios WHERE email = :e");
+
+        $sql->bindValue(":e",$email);
+        $sql->execute();
+
+        if($sql->rowCount() > 0) 
+        {
+            return false; // Já está cadastrada
+        }
+        else
+        {
+            // Caso não, cadastrar
+            $sql = $conexao->prepare("INSERT INTO usuarios (nome, telefone, email, senha) VALUES (:n, :t, :e, :s)");
+            $sql->bindValue(":n",$nome);
+            $sql->bindValue(":t",$telefone);
+            $sql->bindValue(":e",$email);
+            $sql->bindValue(":s",$senha);
+            $sql->execute();
+            return true;
+        }
+
+
+
+    }
+
+    public function login($email, $senha)
+    {
+        global $conexao;
+    }
+}
+
+?>
