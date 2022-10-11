a585ec2de469854d7c96bcef765c49ba610da438
mateus040 <mateusgabrielmoreno264@gmail.com>
2022-10-11 15:38:47 -0300

    finalizando tela de login

--- login.php
+++ login.php
@@ -8,18 +8,26 @@
     <link rel="stylesheet" href="css/style_login.css">
 </head>
 <body>
-    
-    <div class="corpo-form">
-        <h1> Fazer Login </h1>
-        <form method="POST" action="processamento.php">
 
-            <input type="email" placeholder="Usuário">
-            <input type="password" placeholder="Senha">
-            <input type="submit" value="ACESSAR">
-            <a href="">Ainda não se registrou?<strong> Cadastre-se! </strong> </a>
-
-        </form>
+    <div class="main-login">
+        <div class="left-login">
+            <img src="images/logo1.png" class="left-login-image">
+        </div>
+        <div class="right-login">
+            <div class="card-login">
+                <h1>LOGIN</h1>
+                <div class="textfield">
+                    <label for="usuario">Usuário</label>
+                    <input type="text" name="usuario" placeholder="Usuário">
+                </div>
+                <div class="textfield">
+                    <label for="senha">Senha</label>
+                    <input type="password" name="senha" placeholder="Senha">
+                </div>
+                <button type="submit" class="btn-login">Login</button>
+            </div>
+        </div>
     </div>
-
+    
 </body>
 </html>
