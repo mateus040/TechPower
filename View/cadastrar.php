dc4730af14b9784ff5df25e24140d3ba9c7cd8f8
mateus040 <mateusgabrielmoreno264@gmail.com>
2022-10-12 13:31:48 -0300

    adicionando verificação

--- View/cadastrar.php
+++ View/cadastrar.php
@@ -1,3 +1,9 @@
+<?php
+    require_once 'classes/usuarios.php';
+    $u = new Usuario;
+?>
+
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -39,6 +45,54 @@
                 </div>
         </div>
     </form>
+
+    <?php
+
+        // Verificar se clicou no botão
+        if(isset($_POST['nome'])) 
+        {
+            $nome = addslashes($_POST['nome']);
+            $telefone = addslashes(($_POST['telefone']));
+            $email = addslashes($_POST['email']);
+            $senha = addslashes($_POST['senha']);
+            $confirmarSenha = addslashes($_POST['confSenha']);
+
+            // Verificar se está preechido
+            if(!empty($nome) && !empty($telefone) && !empty($email) && !empty($senha) && !empty($confirmarSenha))
+            {
+                $u->__construct("db_techpower", "localhost:3307", "root", "etecjau");
+
+                if($u->msgErro == "") // Se está tudo OK
+                {
+                    if($senha == $confirmarSenha)
+                    {
+                        if($u->cadastrar($nome,$telefone,$email,$senha))
+                        {
+                            echo "Email já cadastrado!";
+                        }
+                        else
+                        {
+                            echo "Cadastrado com sucesso! Acesse para entrar!";
+                        }
+                    }
+                    else
+                    {
+                        echo "Senha e confirmar senha não correspondem!";
+                    }
+                    
+                }
+                else
+                {
+                    echo "Erro ".$u->msgErro;
+                }
+            }
+            else
+            {
+                echo "Preencha todos os campos!";
+            }
+        }
+
+    ?>
 
 </body>
 </html>
