60c6295c7a0994d8592cf50627c3a0e874713da9
mateus040 <mateusgabrielmoreno264@gmail.com>
2022-10-17 21:56:31 -0300

    alterando texto e imagem

--- View/home.php
+++ View/home.php
@@ -92,7 +92,7 @@
     <section class="home" id="home">
 
         <div class="content">
-            <h3>fresh coffee in the morning</h3>
+            <h3>placas de vídeo com até 30% de desconto</h3>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat labore, sint cupiditate distinctio tempora reiciendis.</p>
             <a href="#" class="btn">get yours now</a>
         </div>
