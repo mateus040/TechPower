9a13b8836e440f0f820209a651113034abbe2e63
mateus040 <mateusgabrielmoreno264@gmail.com>
2022-10-27 16:14:26 -0300

    adicionando carrinho

--- View/home.php
+++ View/home.php
@@ -42,7 +42,7 @@
 
         <div class="icons">
             <div class="fas fa-search" id="search-btn"></div>
-            <!-- <div class="fas fa-shopping-cart" id="cart-btn"></div> -->
+            <div class="fas fa-shopping-cart" id="cart-btn"></div>
             <div class="fas fa-bars" id="menu-btn"></div>
         </div>
 
